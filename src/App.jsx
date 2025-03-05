929f6724131602a3e0314390dc8ddf5f39e432ae
big-billy <yusufabiola555@gmail.com>
2025-03-05 09:11:39 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <header className='bg-[red] h-[100px] fixed top-0 left-0 right-0' />
+      
+    </>
+  )
+}
+
+export default App
